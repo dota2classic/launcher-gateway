160cf3f42033eac99e410f89c3d7c648c5505b46
itachi <enchantinggg4@gmail.com>
2020-11-24 13:17:04 +0300

    Sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { REDIS_PASSWORD, REDIS_URL } from './config/env';
+import { isDev, REDIS_PASSWORD, REDIS_URL } from './config/env';
 import { GatewayProviders } from './launcher-gateway';
 import { GatewayController } from './gateway.controller';
 import { LauncherDeliver } from './socket/launcher.deliver';
@@ -9,9 +9,17 @@ import { GatewayService } from './gateway.service';
 import { AuthGateway } from './socket/gateway/auth.gateway';
 import { QueueGateway } from './socket/gateway/queue.gateway';
 import { PartyGateway } from './socket/gateway/party.gateway';
+import { SentryModule } from '@ntegral/nestjs-sentry';
 
 @Module({
   imports: [
+    SentryModule.forRoot({
+      dsn:
+        "https://d0b5e989af4848cf963117815f5a129a@o435989.ingest.sentry.io/5530377",
+      debug: false,
+      environment: isDev ? "dev" : "production",
+      logLevel: 2, //based on sentry.io loglevel //
+    }),
     CqrsModule,
     ClientsModule.register([
       {
